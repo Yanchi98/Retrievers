8e9418bd8d17755d5517f48acb376b177839e83f
yanchiliu <837115713@qq.com>
2024-10-15 20:39:20 +0800

    embedding retriever

--- EmbeddingHelper/embedding_api.py
+++ EmbeddingHelper/embedding_api.py
@@ -6,7 +6,7 @@ from config import embedding_server_url, embedding_server_port
 
 @retry(exceptions=Exception, tries=3, max_delay=20)
 def get_embedding(req_text: str):
-    url = f'{embedding_server_url}:{embedding_server_port}/embedding'
+    url = f'http://{embedding_server_url}:{embedding_server_port}/embedding'
     headers = {'Content-Type': 'application/json'}
     payload = json.dumps({"text": req_text})
     new_req = requests.request("POST", url, headers=headers, data=payload)
