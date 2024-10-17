e6a3661b18565fa19b127a203eee1d3b5f9feb1b
Yan <39321624+Yanchi98@users.noreply.github.com>
2024-10-17 09:57:27 +0800

    Update add_corpus.py

--- preprocess/add_corpus.py
+++ preprocess/add_corpus.py
@@ -6,7 +6,7 @@ with open(corpus_path, mode='r', encoding='utf-8') as f:
 
 corpus = content['corpus']
 queries = list(content['queries'].values())
-# query_relevant_docs: {query}:node_id}
+# query_relevant_docs: {query:node_id}
 query_relevant_docs = {content['queries'][k]: v for k, v in content['relevant_docs'].items()}
 node_id_text_mapping = content['corpus']
 text_node_id_mapping = {v: k for k, v in node_id_text_mapping.items()}
